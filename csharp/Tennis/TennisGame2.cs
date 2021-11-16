85d5a64acd21c8327df0d375d7ebcb00cce8467f
Oscar Herrero <oherrero@aidacanarias.com>
2021-11-16 11:03:08 +0100

    Create class ScoredBoard, Player and Point

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,63 +1,76 @@
+using System;
+using System.Drawing;
 using System.Runtime.CompilerServices;
 
 namespace Tennis
 {
-    public class TennisGame2 : ITennisGame
+    public class ScoreBoard
     {
-        private int player1Point;
-        private int player2Point;
+        private readonly Player player1;
+        private readonly Player player2;
+
+        public ScoreBoard(string player1, string player2)
+        {
+            this.player1 = new Player(player1);
+            this.player2 = new Player(player2);
+        }
 
         public string GetScore()
         {
-            var score = "";
-            if (player1Point == player2Point && player1Point < 3)
-            {
-                score = EqualsScore();
-            }
-            if (player1Point == player2Point && player1Point > 2)
-                score = "Deuce";
-
-            if (player1Point > 0 && player2Point == 0)
-            {
-                score = PointToScore(player1Point) +"-" + PointToScore(player2Point);
-            }
-            if (player2Point > 0 && player1Point == 0)
-            {
-                score = PointToScore(player1Point) + "-" + PointToScore(player2Point);
-            }
-
-            if (player1Point > player2Point && player1Point < 4)
-            {
-                score = PointToScore(player1Point) + "-" + PointToScore(player2Point);
-            }
-            if (player2Point > player1Point && player2Point < 4)
-            {
-                score = PointToScore(player1Point) + "-" + PointToScore(player2Point);
-            }
-
-            if (player1Point > player2Point && player2Point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (player2Point > player1Point && player1Point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2)
-            {
-                score = "Win for player2";
-            }
-            return score;
+            int pointPlayer1 = player1.GetPoint();
+            int pointPlayer2 = player2.GetPoint();
+            if (pointPlayer1 == pointPlayer2) return EqualsScore();
+            if(pointPlayer1 > 3 || pointPlayer2 > 3) return AdvantageOrWin(pointPlayer1, pointPlayer2);
+            return player1.GetScore() + "-" + player2.GetScore();
         }
 
-        private string PointToScore(int point) =>
-            point switch
+        public void WonPoint(string player)
+        {
+            if (player1.IsPlayer(player))
+                player1.AddPoint();
+            else
+                player2.AddPoint();
+        }
+
+        private string EqualsScore() => player1.GetPoint() > 2 ? "Deuce" : player1.GetScore() + "-All";
+       
+        private string AdvantageOrWin(int pointPlayer1, int pointPlayer2) =>
+            (Math.Abs(pointPlayer1 - pointPlayer2) >= 2 ? "Win for " : "Advantage ")
+            + (pointPlayer1 > pointPlayer2 ? player1.GetName() : player2.GetName());
+    }
+
+    public class Player
+    {
+        private string Name { get; }
+        private Point Point { get; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Point = new Point();
+        }
+
+        public void AddPoint() => Point.Add();
+
+        public int GetPoint() => Point.GetPoint();
+
+        public string GetScore() => Point.ToString();
+
+        public bool IsPlayer(string player) => Name == player;
+
+        public object GetName() => Name;
+    }
+
+    public class Point
+    {
+        private int Value { get; set; }
+
+        public void Add() => Value++;
+
+        public int GetPoint() => Value;
+
+        public override string ToString() =>
+            Value switch
             {
                 0 => "Love",
                 1 => "Fifteen",
@@ -65,10 +78,17 @@ namespace Tennis
                 3 => "Forty",
                 _ => ""
             };
+    }
+    
+    public class TennisGame2 : ITennisGame
+    {
+        private readonly ScoreBoard scoreBoard;
 
-        private string EqualsScore() => PointToScore(player1Point) + "-All";
+        public TennisGame2() => scoreBoard = new ScoreBoard("player1", "player2");
 
-        public void WonPoint(string player) => _ = player == "player1" ? player1Point++ : player2Point++;
+        public string GetScore() => scoreBoard.GetScore();
+
+        public void WonPoint(string player) => scoreBoard.WonPoint(player);
     }
 }
 
